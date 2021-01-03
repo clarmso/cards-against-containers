d3186bca67d1da5bbbc75817c4efb2acd67d402a
clarmso <clare.m.so+github@gmail.com>
2021-01-03 01:11:34 -0500

    Attempt to check in a file that requires prettier cleanup

--- ui/cypress/integration/e2e.spec.js
+++ ui/cypress/integration/e2e.spec.js
@@ -16,6 +16,7 @@ describe("Sanity tests", () => {
     cy.contains("button", "Refresh").click()
     cy.wait("@v1APIquestion")
     cy.wait("@v1APIanswer")
+
     cy.contains("button", "Refresh").should("be.enabled")
     cy.get("[data-cy*='question-']").should("be.visible")
     cy.get("[data-cy*='answer-']").should("be.visible")
